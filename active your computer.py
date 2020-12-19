3d7e772e10ebf492c07607df6291d30699230bee
OM PRAKASH <opytc2@gmail.com>
2020-12-20 00:38:58 +0530

    First commit

---
+++ active your computer.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+pyautogui.FAILSAFE = False
+
+while True:
+    time.sleep(15)
+    for i in range(0,100):
+        pyautogui.moveTo(0, i * 5)
+
+    for i in range(0,2):
+        pyautogui.press('shift')
+
+        
